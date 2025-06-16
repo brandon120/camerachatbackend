f78d4edd4e4c6b6d10c1f0481114ca88959c1bbd
Brandon Monroe <brandonjmonroe@gmail.com>
2025-06-16 16:13:28 -0500

    Update socketHandler.js

--- src/sockets/socketHandler.js
+++ src/sockets/socketHandler.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken';
 import frameBuffer from '../utils/frameBuffer.js';
 
+// Store active chat rooms and their participants
+const activeChats = new Map();
+
 export const initializeSocket = (io) => {
   io.use(async (socket, next) => {
     try {
@@ -19,6 +22,13 @@ export const initializeSocket = (io) => {
   io.on('connection', (socket) => {
     console.log('User connected:', socket.userId);
 
+    // Join any existing chat rooms for this user
+    activeChats.forEach((participants, roomId) => {
+      if (participants.includes(socket.userId)) {
+        socket.join(roomId);
+      }
+    });
+
     socket.on('chat_request', async ({ receiverId }) => {
       const room = `${socket.userId}-${receiverId}`;
       socket.join(room);
@@ -30,7 +40,22 @@ export const initializeSocket = (io) => {
 
     socket.on('chat_response', ({ room, accepted }) => {
       if (accepted) {
-        io.to(room).emit('chat_started', { room });
+        // Store the chat room and its participants
+        const [user1, user2] = room.split('-');
+        activeChats.set(room, [user1, user2]);
+
+        // Notify both users that chat has started
+        io.to(room).emit('chat_started', { 
+          room,
+          participants: [user1, user2]
+        });
+
+        // Join both users to the room
+        io.sockets.sockets.forEach(s => {
+          if (s.userId === user1 || s.userId === user2) {
+            s.join(room);
+          }
+        });
       } else {
         io.to(room).emit('chat_rejected');
       }
@@ -38,18 +63,29 @@ export const initializeSocket = (io) => {
 
     socket.on('stream_frame', ({ frame }) => {
       frameBuffer.updateFrame(socket.userId, frame);
-      const room = socket.rooms.values().next().value;
-      if (room) {
-        socket.to(room).emit('stream_frame', {
-          userId: socket.userId,
-          frame
-        });
-      }
+      
+      // Send frame to all chat rooms the user is in
+      socket.rooms.forEach(room => {
+        if (room !== socket.id) { // Skip the socket's own room
+          socket.to(room).emit('stream_frame', {
+            userId: socket.userId,
+            frame
+          });
+        }
+      });
     });
 
     socket.on('disconnect', () => {
       console.log('User disconnected:', socket.userId);
       frameBuffer.removeFrame(socket.userId);
+
+      // Clean up chat rooms when user disconnects
+      activeChats.forEach((participants, room) => {
+        if (participants.includes(socket.userId)) {
+          io.to(room).emit('user_disconnected', { userId: socket.userId });
+          activeChats.delete(room);
+        }
+      });
     });
   });
 }; 
