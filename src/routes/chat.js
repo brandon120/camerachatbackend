864c5d1279ddf1f13f961c59ed074c4675abe928
Brandon Monroe <brandonjmonroe@gmail.com>
2025-06-16 16:52:25 -0500

    updating chat requests to allow deleting previous requests

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { sendChatRequest, respondToChatRequest, getChatRequests } from '../controllers/chatController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
+import { sendChatRequest, respondToChatRequest, getChatRequests, getSentChatRequests, deleteChatRequest } from '../controllers/chatController.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.use(authMiddleware);
-
-router.post('/request', sendChatRequest);
-router.post('/respond', respondToChatRequest);
-router.get('/requests', getChatRequests);
+router.post('/request', authenticateToken, sendChatRequest);
+router.post('/respond', authenticateToken, respondToChatRequest);
+router.get('/requests', authenticateToken, getChatRequests);
+router.get('/sent-requests', authenticateToken, getSentChatRequests);
+router.delete('/request/:requestId', authenticateToken, deleteChatRequest);
 
 export default router; 
